77439f3a59b5ffc54cfff29d59330591de3bd1a9
asgk912 <asgk912@gmail.com>
2019-12-31 11:20:58 -0800

    implement youtube search feature

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,18 @@
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  var server = 'https://www.googleapis.com/youtube/v3/search';
+  var {query, max, key} = options;
+
+
+  $.ajax({
+    url: server,
+    type: 'GET',
+    data: {'part': 'snippet', 'type': 'video', 'maxResults': max, 'q': query, 'key': key},
+    success: (data) => callback(data.items),
+    dataType: 'json'
+  });
 };
+
 
 export default searchYouTube;
