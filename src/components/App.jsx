1a532440a94173a07feb8fa8c6fef262842024b0
Hack Reactor Student <student@hackreactor.com>
2019-12-30 21:47:51 -0800

    implement video click feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,69 @@ import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer video={exampleVideoData[0]}/>
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos={exampleVideoData}/>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoList: exampleVideoData,
+      videoPlayer: exampleVideoData[0]
+    };
+  }
+
+  playOnClick(event) {
+    if (event.target.className === "video-list-entry-title") {
+
+      for (var i = 0; i < this.state.videoList.length; i++) {
+
+        if (event.target.innerText === this.state.videoList[i].snippet.title) {
+          this.setState({
+            videoPlayer: exampleVideoData[i]
+          });
+          break;
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.videoPlayer}/>
+          </div>
+          <div className="col-md-5" onClick={this.playOnClick.bind(this)}>
+            <VideoList videos={this.state.videoList}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={exampleVideoData[0]}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
