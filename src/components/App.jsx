a244bd0dedc836466f9e82fabe9fe0b02e6f495a
Hack Reactor Student <student@hackreactor.com>
2019-12-31 16:10:48 -0800

    partially implement live-search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
+
 
 // var App = () => (
 //   <div>
@@ -25,19 +29,55 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      videoList: exampleVideoData,
-      videoPlayer: exampleVideoData[0]
+      videoList: [],
+      videoPlayer: {id: {videoId: ''}, snippet: {}},
     };
   }
 
+  componentDidMount() {
+    // intial option for youtub serach
+    var options = {key: YOUTUBE_API_KEY, query: 'react', max: 5};
+
+    // callback function that is passed to searchYouTube
+    var cbSetState = function(data) {
+      this.setState({
+        videoList: data,
+        videoPlayer: data[0]
+      });
+    };
+
+    // binding this to App
+    var bindedCBSetState = cbSetState.bind(this);
+    var bindedSearchYouTube = this.props.searchYouTube.bind(this);
+
+    // initial video load
+    bindedSearchYouTube(options, bindedCBSetState);
+
+    // check input every 1 second and render page accordingly
+    setInterval(() => {
+      var searchWord = document.getElementsByClassName('form-control')[0].value;
+      if (options.query !== undefined && options.query !== searchWord) {
+        options.query = searchWord;
+        bindedSearchYouTube(options, bindedCBSetState);
+      }
+    }, 500);
+
+    /* test on fetching input data with set interval
+    setInterval(() => {
+      var searchWord = document.getElementsByClassName("form-control")[0].value;
+      console.log(bindedSearchYouTube);
+    }, 1000);
+    */
+  }
+
   playOnClick(event) {
-    if (event.target.className === "video-list-entry-title") {
+    if (event.target.className === 'video-list-entry-title') {
 
       for (var i = 0; i < this.state.videoList.length; i++) {
 
         if (event.target.innerText === this.state.videoList[i].snippet.title) {
           this.setState({
-            videoPlayer: exampleVideoData[i]
+            videoPlayer: this.state.videoList[i]
           });
           break;
         }
@@ -50,7 +90,7 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search />
           </div>
         </nav>
         <div className="row">
